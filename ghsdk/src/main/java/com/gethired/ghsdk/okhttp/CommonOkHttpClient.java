a514867514dbb56eddaf515579a4bf6c50dc1f68
Hawk Song <savokisong@gmail.com>
2019-08-08 17:29:08 -0700

    temp save

---
+++ ghsdk/src/main/java/com/gethired/ghsdk/okhttp/CommonOkHttpClient.java
@@ -0,0 +1,41 @@
+package com.gethired.ghsdk.okhttp;
+
+import java.util.concurrent.TimeUnit;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * @function send request, configure params, support https
+ */
+public class CommonOkHttpClient {
+
+    private static final int TIME_OUT = 30; // time out
+    private static OkHttpClient mOkhttpClient;
+
+    // configure params for client
+    static {
+
+        // create builder for client
+        OkHttpClient.Builder okHttpBuilder = new OkHttpClient.Builder();
+
+        //set up timeout
+        okHttpBuilder.connectTimeout(TIME_OUT, TimeUnit.SECONDS);
+        okHttpBuilder.readTimeout(TIME_OUT, TimeUnit.SECONDS);
+        okHttpBuilder.writeTimeout(TIME_OUT, TimeUnit.SECONDS);
+
+        okHttpBuilder.followRedirects(true);
+
+        //https support
+        okHttpBuilder.hostnameVerifier(new HostnameVerifier() {
+            @Override
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        });
+
+        okHttpBuilder.sslSocketFactory()
+    }
+}
